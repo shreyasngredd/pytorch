b40b3f8304c4309199752b4ac73c32b310229593
ilia-cher <iliacher@fb.com>
2019-05-02 18:38:54 -0700

    Native ATen/Parallel backend
    Summary:
    Implementing ATen/Parallel withtout OpenMP using native thread pool
    
    Test Plan:
    PARALLEL_BACKEND=NATIVE BLAS=MKL USE_MKLDNN=1 USE_OPENCV=1 USE_FFMPEG=1
    python setup.py develop --cmake
    pytest -s -v test/test_torch.py::TestTorch
    
    gh-metadata: pytorch pytorch 20087 gh/ilia-cher/15/head

--- aten/src/ATen/PTThreadPool.h
+++ aten/src/ATen/PTThreadPool.h
@@ -4,6 +4,16 @@
 
 namespace at {
 
+// Checks external number of threads setting and returns
+// default value (hardware_concurrency()) if it's negative
+inline size_t check_and_get_pool_size(int nthreads) {
+  if (nthreads > 0) {
+    return nthreads;
+  } else {
+    return std::thread::hardware_concurrency();
+  }
+}
+
 class CAFFE2_API PTThreadPool : public c10::ThreadPool {
  public:
   explicit PTThreadPool(
